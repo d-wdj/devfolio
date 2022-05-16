ecf695dba1f57879a6400542fea76735c9d35705
Dimitri Wirjowerdojo <25409447+d-wdj@users.noreply.github.com>
2022-05-16 15:26:22 +0800

    test subtitle on experience section

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -14,7 +14,7 @@ const NotFoundPage = () => (
     <SEO title="Not found" />
     <h1 className={classes.title}>404: Not Found</h1>
     <p>
-      Dang! You just hit a route that doesn't exist :\{' '}
+      Dang! You just hit a route that doesn't exist :/{' '}
       <Link className={classes.link} to="/">
         Return to safety
       </Link>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -54,6 +54,7 @@ export const pageQuery = graphql`
         }
         experience {
           name
+          subtitle
           description
           link
         }
