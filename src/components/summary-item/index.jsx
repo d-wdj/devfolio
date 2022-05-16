20cb80b58575ea3071b52d6e631033a30478d10b
Dimitri Wirjowerdojo <25409447+d-wdj@users.noreply.github.com>
2022-05-16 15:58:10 +0800

    test subtitle on experience section

--- src/components/summary-item/index.jsx
+++ src/components/summary-item/index.jsx
@@ -8,7 +8,7 @@ const classes = {
   description: 'text-md text-gray-600 font-light',
 };
 
-const SummaryItem = ({ name, description, link = false, internal = false }) => {
+const SummaryItem = ({ name, subtitle = false, description, link = false, internal = false }) => {
   let linkContent;
   if (internal) {
     linkContent = <Link to={link}>{name}</Link>;
